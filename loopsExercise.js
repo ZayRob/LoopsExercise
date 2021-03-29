9042c3493b4e9c39cdc3e820e0c795b0621eeea0
Isaiah Robinson <isaiahrobinson@KCCCs-MacBook-Pro-8.local>
2021-03-29 03:08:31 -0400

    Completed Loops Exercise clear

---
+++ loopsExercise.js
@@ -0,0 +1,29 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+  }
+ 
+  console.log("=========");
+  
+  for (i = 5; i <= 25; i+=4){
+    console.log(i);
+  }
+  
+  console.log("=========");
+
+  const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+  ];
+  
+  for (n of wizards){
+    console.log(n);
+  }
+  
+  let harryPotterMovies = 0;
+  
+  while (harryPotterMovies <= 7){
+    harryPotterMovies++;
+    console.log(harryPotterMovies);
+  }
+ 
